018eb7a4d21afe95af140f6f1558515ceee230c7
luiz-chiq <116838721+luiz-chiq@users.noreply.github.com>
2022-10-30 17:44:37 -0300

    Add files via upload

--- petshop/Funcionarios.php
+++ petshop/Funcionarios.php
@@ -46,6 +46,8 @@
                 ?>
                 </tbody>
                 </table>
+                <br><br>
+                <a href='index.php'>Voltar ao index</a>
                 
                 
                 
